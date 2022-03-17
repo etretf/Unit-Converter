4a5d181a744e6ad434c25231c132e4162a84c997
etretf <77692774+etretf@users.noreply.github.com>
2022-03-16 22:10:54 -0400

    Wrote distance switch statement

--- distance.js
+++ distance.js
@@ -20,45 +20,89 @@ document.getElementById("mile").addEventListener("input", mass);
 
 
 function distanceConvert() {
-    console.log("mass");
+    console.log("distance");
     inputId = event.target.id;
     inputValue = parseFloat(event.target.value);
     switch(inputId) {
-        case "grams":
-            console.log("grams");
-            kilograms.value = inputValue/1000;
-            tons.value = inputValue*1.1023e-6;
-            pounds.value = inputValue*0.002204623;
-            ounces.value = inputValue*0.03527396;
+        case "millimeter":
+            console.log("millimeter");
+            centimeter.value = inputValue*0.1;
+            meter.value = inputValue*0.001;
+            kilometer.value = inputValue*0.000001;
+            inch.value = inputValue*0.039370079;
+            feet.value= inputValue*0.0032808399;
+            yard.value= inputValue*0.0010936133;
+            mile.value= inputValue*6.213712e-7;
             break;
-        case "kilograms":
-            console.log("kilograms");
-            grams.value = (inputValue*1000).toFixed(2);
-            tons.value = inputValue*0.001102;
-            pounds.value = inputValue*2.204623;
-            ounces.value = inputValue*35.27396;
+        case "centimeter":
+            console.log("centimeter");
+            millimeter.value = inputValue*10;
+            meter.value = inputValue*0.01;
+            kilometer.value = inputValue*0.00001;
+            inch.value = inputValue*0.39370079;
+            feet.value= inputValue*0.032808399;
+            yard.value= inputValue*0.010936133;
+            mile.value= inputValue*6.213712e-6;
             break;
-        case "tons":
-            console.log("tons");
-            grams.value = inputValue*1000; 
-            kilograms.value = inputValue*100;
-            pounds.value = (inputValue*2204.6226).toFixed(2);
-            ounces.value = inputValue*35273.962;
-            console.log(inputValue);
+        case "meter":
+            console.log("meter");
+            millimeter.value = inputValue*1000;
+            centimeter.value = inputValue*100;
+            kilometer.value = inputValue*0.001;
+            inch.value = inputValue*39.370079;
+            feet.value= inputValue*3.2808399;
+            yard.value= inputValue*1.0936133;
+            mile.value= inputValue*6.213712e-4;
             break;  
-        case "pounds":
-            console.log("pounds");
-            grams.value = inputValue*453.59237;
-            kilograms.value = inputValue*0.45359237;
-            tons.value = inputValue*0.00045359237;
-            ounces.value = inputValue*16;
+        case "kilometer":
+            console.log("kilometer");
+            millimeter.value = inputValue*1000000;
+            centimeter.value = inputValue*100000;
+            meter.value = inputValue*1000;
+            inch.value = inputValue*39370.079;
+            feet.value= inputValue*3280.8399;
+            yard.value= inputValue*1093.6133;
+            mile.value= inputValue*0.6213712;
             break;
-        case "ounces":
-            console.log("ounces");
-            grams.value = inputValue*28.349523;
-            kilograms.value = inputValue*0.028349523;
-            tons.value = inputValue*2.8349523e-5;
-            pounds.value = inputValue*0.0625 ;
+        case "inch":
+             console.log("inch");
+            millimeter.value = inputValue*25.4;
+            centimeter.value = inputValue*2.54;
+            meter.value = inputValue*0.0254;
+            kilometer.value = inputValue*0.0000254;
+            feet.value= inputValue*0.083333333;
+            yard.value= inputValue*0.0277777778;
+            mile.value= inputValue*0.000015782828;
+            break;
+        case "feet":
+             console.log("feet");
+            millimeter.value = inputValue*304.8;
+            centimeter.value = inputValue*30.48;
+            meter.value = inputValue*0.3048;
+            kilometer.value = inputValue*0.0003048;
+            inch.value= inputValue*12;
+            yard.value= inputValue*0.33333333333;
+            mile.value= inputValue*0.00018939394;
+            break;
+        case "yard":
+             console.log("yard");
+            millimeter.value = inputValue*914.4;
+            centimeter.value = inputValue*91.44;
+            meter.value = inputValue*0.9144;
+            kilometer.value = inputValue*0.0009144;
+            inch.value= inputValue*36;
+            feet.value= inputValue*3;
+            mile.value= inputValue*0.00056818182;
+            break;
+        case "mile":
+             console.log("mile");
+            millimeter.value = inputValue*1609344;
+            centimeter.value = inputValue*160934.4;
+            meter.value = inputValue*1609.344;
+            kilometer.value = inputValue*1.609344;
+            inch.value= inputValue*63360;
+            feet.value= inputValue*5280;
+            yard.value= inputValue*1760;
             break;
     }
 }
