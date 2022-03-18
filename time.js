14dffbc98b3e2c8d4cd349f5f2c1be304affa36c
etretf <77692774+etretf@users.noreply.github.com>
2022-03-18 09:40:47 -0400

    Completed all math operations for time conversions

--- time.js
+++ time.js
@@ -29,59 +29,59 @@ function timeConvert() {
     switch(inputId) {
         case "second":
             minute.value= inputValue/60;
-            hour.value= inputValue;
-            day.value=inputValue;
-            week.value=inputValue;
-            month.value=inputValue;
-            year.value=inputValue;
+            hour.value= inputValue/3600;
+            day.value=inputValue/86400;
+            week.value=inputValue/604800;
+            month.value=inputValue/2629800;
+            year.value=inputValue/31557600;
             break;
         case "minute":
             second.value=inputValue*60;
-            hour.value=inputValue;
-            day.value=inputValue;
-            week.value=inputValue;
-            month.value=inputValue;
-            year.value=inputValue;
+            hour.value=inputValue/60;
+            day.value=inputValue/1440;
+            week.value=inputValue/10080;
+            month.value=inputValue/43830;
+            year.value=inputValue/525960;
             break;
         case "hour":
-            second.value=inputValue*60;
-            minute.value=inputValue;
-            day.value=inputValue;
-            week.value=inputValue;
-            month.value=inputValue;
-            year.value=inputValue;
+            second.value=inputValue*3600;
+            minute.value=inputValue*60;
+            day.value=inputValue/24
+            week.value=inputValue/168;
+            month.value=inputValue/730.5;
+            year.value=inputValue/8766;
             break;
         case "day":
-            second.value=inputValue;
-            minute.value=inputValue;
-            hour.value=inputValue;
-            week.value=inputValue;
-            month.value=inputValue;
-            year.value=inputValue;
+            second.value=inputValue*604800;
+            minute.value=inputValue*10080;
+            hour.value=inputValue*168;
+            week.value=inputValue/7;
+            month.value=inputValue/30.4375;
+            year.value=inputValue/365.25;
             break;
         case "week":
-            second.value=inputValue;
-            minute.value=inputValue;
-            hour.value=inputValue;
-            day.value=inputValue;
-            month.value=inputValue;
-            year.value=inputValue;
+            second.value=inputValue*604800;
+            minute.value=inputValue*10080;
+            hour.value=inputValue*168;
+            day.value=inputValue*7;
+            month.value=inputValue/4.34524;
+            year.value=inputValue/52.17857;
             break;
         case "month":
-            second.value=inputValue;
-            minute.value=inputValue;
-            hour.value=inputValue;
-            day.value=inputValue;
-            week.value=inputValue;
-            year.value=inputValue;
+            second.value=inputValue*2629800;
+            minute.value=inputValue*43830;
+            hour.value=inputValue*730.5;
+            day.value=inputValue*30.4375;
+            week.value=inputValue*4.348214;
+            year.value=inputValue/12;
             break;
         case "year":
-            second.value=inputValue;
-            minute.value=inputValue;
-            hour.value=inputValue;
-            day.value=inputValue;
-            week.value=inputValue;
-            month.value=inputValue;
+            second.value=inputValue*31557600;
+            minute.value=inputValue*525960;
+            hour.value=inputValue*8766;
+            day.value=inputValue*365.25;
+            week.value=inputValue*52.17857;
+            month.value=inputValue*12;
             break;
             
     }
